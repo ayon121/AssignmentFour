3f27df3338155b0818c47585613fd5573e66ecc7
ayon121 <borshonsaha1234@gmail.com>
2025-07-03 15:51:30 +0600

    footer and navbar  function added

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css'
 import { Button } from './components/ui/button'
 import { ModeToggle } from './components/mode-toggle'
 import 'react-toastify/dist/ReactToastify.css'
+import { DropdownMenuComponent } from './components/module/DropDownMenuComponent'
 
 function App() {
 
@@ -13,18 +14,34 @@ function App() {
       <div className='max-w-7xl mx-auto flex flex-row gap-4 items-center justify-between border-2 border-black dark:border-white rounded-2xl py-2 px-0.5 md:px-1.5 mb-5'>
         <div className='flex flex-row gap-3 items-center'>
           <ModeToggle></ModeToggle>
+          <div className='block lg:hidden'>
+            <DropdownMenuComponent></DropdownMenuComponent>
+          </div>
           <h1 className=' text-xs md:text-xl font-extrabold uppercase dark:text-white w-full'>Book Management</h1>
         </div>
-        <div className='flex flex-row gap-3 items-center'>
-          <Button className='px-2 py-1 text-xs sm:px-3 sm:py-1.5 sm:text-sm'><Link to="/">Home</Link></Button>
-          <Button className='px-2 py-1 text-xs sm:px-3 sm:py-1.5 sm:text-sm'><Link to="/books">Book</Link></Button>
-          <Button className='px-2 py-1 text-xs sm:px-3 sm:py-1.5 sm:text-sm'><Link to="/create-book">Add Book</Link></Button>
+        <div className='hidden  lg:flex flex-row gap-1 items-center '>
+          <Button className='px-2 py-1 text-xs sm:px-2 sm:py-1.5 sm:text-sm'><Link to="/">Home</Link></Button>
+          <Button className='px-2 py-1 text-xs sm:px-2 sm:py-1.5 sm:text-sm'><Link to="/books">All Books</Link></Button>
+          <Button className='px-2 py-1 text-xs sm:px-2 sm:py-1.5 sm:text-sm'><Link to="/create-book">Add Book</Link></Button>
+          <Button className='px-2 py-1 text-xs sm:px-2 sm:py-1.5 sm:text-sm'><Link to="/create-book">Borrow Summary</Link></Button>
         </div>
+
 
 
       </div>
-      
-      <Outlet></Outlet>
+
+      <div className='min-h-screen'>
+        <Outlet></Outlet>
+      </div>
+      {/* footer */}
+      <footer className="max-w-7xl mx-auto border-t-2 border-black dark:border-white mt-10 pt-4 pb-6 px-2 md:px-4 text-center rounded-t-2xl shadow-inner">
+        <p className="text-lg sm:text-sm font-bold text-muted-foreground">
+          © {new Date().getFullYear()} Book Management. All rights reserved.
+        </p>
+        <p className="text-xs text-muted-foreground mt-1 font-bold">
+          Developed by <a href="https://github.com/ayon121" className="underline hover:text-black dark:hover:text-white">Ayon Saha</a>
+        </p>
+      </footer>
     </>
   )
 }
---
+++ src/components/module/DropDownMenuComponent.tsx
@@ -0,0 +1,37 @@
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Link } from "react-router"
+
+export function DropdownMenuComponent() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline">Menu</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="start">
+        <DropdownMenuLabel className="text-xl font-bold uppercase">All Options</DropdownMenuLabel>
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <Button className='px-2 py-1 text-xs sm:px-2 sm:py-1.5 sm:text-sm w-full'><Link to="/">Home</Link></Button>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Button className='px-2 py-1 text-xs sm:px-2 sm:py-1.5 sm:text-sm w-full'><Link to="/books">All Books</Link></Button>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Button className='px-2 py-1 text-xs sm:px-2 sm:py-1.5 sm:text-sm w-full'><Link to="/create-book">Add Book</Link></Button>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Button className='px-2 py-1 text-xs sm:px-2 sm:py-1.5 sm:text-sm w-full'><Link to="/create-book">Borrow Summary</Link></Button>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
